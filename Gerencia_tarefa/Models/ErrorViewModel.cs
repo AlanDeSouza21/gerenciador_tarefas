3a668f01d6f2a3989a3fc2f88052793e9c6f5435
AlanDeSouza21 <as219de@uni9.edu.br>
2026-04-30 08:06:04 -0300

    projeto_gerenciamento_tarefas

---
+++ Gerencia_tarefa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerencia_tarefa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
